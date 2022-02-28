3d14d3b0f4c571388864def7792502b89b776868
Jesus Sevilla <jesus.sevilla.pena@gmail.com>
2022-02-28 10:07:18 -0600

    add back to top button and fix tech logos size

--- app.js
+++ app.js
@@ -2,7 +2,17 @@ const toggle = document.querySelector(".main-nav-toggle");
 const navbar = document.querySelector(".main-navigation");
 const toggleLines = document.querySelectorAll(".main-toggle-line");
 const slideInElements = document.querySelectorAll(".slide-in");
+const backToTop = document.querySelector(".back-to-top");
 
+
+const backToTopFunction = () => {
+
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        backToTop.classList.add("active");
+    } else {
+        backToTop.classList.remove("active");
+    }
+};
 
 toggle.addEventListener("click", () => {
 
@@ -18,4 +28,15 @@ slideInElements.forEach(slideIn => {
     slideIn.addEventListener("animationend", () => {
         slideIn.classList.remove("slide-in");
     });
+});
+
+
+
+window.onscroll = () => {
+    backToTopFunction();
+}
+
+backToTop.addEventListener("click", () => {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
 });
