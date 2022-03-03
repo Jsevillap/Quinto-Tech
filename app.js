ce2f88a152d22e4d9b701928dc2b6fb87df3ae61
Jesus Sevilla <jesus.sevilla.pena@gmail.com>
2022-03-02 22:36:19 -0600

    replace svg and add color scheme changer

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ const
     slideInElements = document.querySelectorAll(".slide-in"),
     backToTop = document.querySelector(".back-to-top"),
     mainHeader = document.querySelector(".main-header"),
-    navLinks = document.querySelectorAll(".link-decoration");
+    navLinks = document.querySelectorAll(".link-decoration"),
+    root = document.querySelector(":root"),
+    colorSelectors = document.querySelectorAll(".color-scheme");
 
 
 const backToTopFunction = () => {
@@ -68,4 +70,37 @@ backToTop.addEventListener("click", () => {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
     document.title = "Quinto Technologies";
+});
+
+const changeColorProperties = (color, colorVariant) => {
+    root.style.setProperty("--main-accent", color);
+    root.style.setProperty("--main-accent-900", colorVariant);
+}
+
+
+colorSelectors.forEach(selector => {
+
+    selector.addEventListener("click", () => {
+
+        switch (selector.dataset.color) {
+            case "blue":
+                changeColorProperties("#2C77D1", "#0C2C77");
+                break;
+            case "purple":
+                changeColorProperties("#AE39E4", "#45135B");
+                break;
+            case "orange":
+                changeColorProperties("#DC3A04", "#6A1808");
+                break;
+            case "pink":
+                changeColorProperties("#E9045C", "#48041E");
+                break;
+            case "green":
+                changeColorProperties("#07B7A6", "#004841");
+                break;
+
+        }
+
+    });
+
 });
